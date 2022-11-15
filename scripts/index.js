710ec581530eb79629e9b82bdc34706883d6f698
OrkhanRakhmetov <orakhmetov@mail.ru>
2022-11-15 19:09:22 +0300

    fix: устранить ошибки после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -3,20 +3,20 @@ const popupCloseButtonElement = popupElement.querySelector('.popup__button-close
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 
 const openPopup = function () {
-    popupElement.classList.add('popup__is-opened');
+    popupElement.classList.add('popup_active');
     nameInput.value = prifileName.textContent;
     jobInput.value = profileJob.textContent;
 }
 const closePopup = function () {
-    popupElement.classList.remove('popup__is-opened');
+    popupElement.classList.remove('popup_active');
 }
 
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 
 let formElement = document.querySelector('.popup__inputs');
-let nameInput = document.querySelector('.popup__input_name-info');
-let jobInput = document.querySelector('.popup__input_job-info');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
 let prifileName = document.querySelector('.profile__title');
 let profileJob = document.querySelector('.profile__subtitle');
 function formSubmitHandler(evt) {
